d8b5d1dcf647a862365727e13c7e0b8176f762c0
fishpog <126018624+flshpog@users.noreply.github.com>
2026-04-06 19:02:20 -0400

    shuffle rock draw order so its not predictable from input

--- src/commands/org/rocks.js
+++ src/commands/org/rocks.js
@@ -39,6 +39,12 @@ module.exports = {
                 });
             }
 
+            // Shuffle the player order so draw order is random
+            for (let i = players.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [players[i], players[j]] = [players[j], players[i]];
+            }
+
             // Randomly decide which player gets the purple rock (eliminated)
             const eliminatedIndex = Math.floor(Math.random() * players.length);
 
